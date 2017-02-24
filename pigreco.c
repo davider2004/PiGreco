5920b52f461bd5b4ec13b75663e9afb1142388ee
Davide Ramondetti <github@ramondettidavide.com>
2017-02-24 22:24:28 +0100

    PiGreco v1 BETA
    First Pigreco v1 BETA code

---
+++ pigreco.c
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+float pg_leib(float n);
+float precisione,p1,p2,ripetere,s;
+int main (int argc, char *argv[])
+{
+   do{cout<<"Inserisci la precisione"<<endl;
+   cin>>precisione;
+   int k=50;
+   do
+    {p1=pg_leib(k);
+    
+    p2=pg_leib(k*2);
+    k=k*2;
+    } while (fabs (p1-p2)>precisione);
+    cout<<"il pigreco vale........."<<endl;
+    cout<<(p1+p2)/2<<endl;;
+    cout<<"le iterazioni fatte sono........"<<endl;
+    cout<<k/2<<endl;
+     cout<<"Ripetere (1-si')"<<endl;
+    cin>>ripetere;
+  }while (ripetere==1);
+   
+}
+
+float pg_leib(float n)
+{float p=1;
+  short int s=-1;
+     for (float i=3; i<=n; i+=2)
+         {p=p+(s/i);
+         s=-s;}
+return p*4;}
