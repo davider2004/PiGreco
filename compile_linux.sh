9dd3a7a98cace943238621cd09d06c8680faba89
Davide Ramondetti <github@ramondettidavide.com>
2017-02-25 14:18:18 +0100

    Created the compiler for Linux

---
+++ compile_linux.sh
@@ -0,0 +1,17 @@
+sudo apt-get install figlet g++ gcc cc
+clear
+figlet "PiGreco"
+echo "a RamondettiDavide.com creation"
+echo "<github.com/davider2004>"
+echo
+echo "Do you want compile PiGreco.cpp? ENTER"
+read
+sudo g++ -std=c++11 -o main *.cpp
+echo
+clear
+figlet "PiGreco"
+echo "a RamondettiDavide.com creation"
+echo "<github.com/davider2004>"
+echo
+echo "Compiling completed"
+echo "Now you can type ./pigreco and run the software"
